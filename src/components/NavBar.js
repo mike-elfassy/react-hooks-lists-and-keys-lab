1a63ee34a722acbb219631186e1b27587c5229b2
Michael Elfassy <mike.elfassy@gmail.com>
2023-02-11 19:39:31 -0500

    commit tree 2b8b3ed4f443cca1e209c87ee0058958c03089fd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkList = links.map((link, index) => <a key={index} href={'#' + link}>{link}</a>)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkList}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyList = technologies.map((technology, index) => <span key={index}>{technology}</span>)
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologyList}
       </div>
     </div>
   );
